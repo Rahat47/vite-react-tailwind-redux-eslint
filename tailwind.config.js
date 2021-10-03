5936eef89999c9ed291c54e23360c252fdd611a4
Rahat47 <djrayhan8@gmail.com>
2021-10-03 15:16:53 +0600

    boilerplate v1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        react: {
+          DEFAULT: '#61dafb'
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['motion-safe']
+    },
+  },
+  plugins: [],
+}
